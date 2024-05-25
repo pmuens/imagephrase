63d828318aa17c976d614ac5da16db80c71d8962
Philipp Muens <philipp@muens.io>
2024-05-25 10:07:00 +0200

    Add support for subcommands

---
+++ cmd/imgp/env.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type env struct {
+	stdout io.Writer
+	stderr io.Writer
+	args   []string
+}
+
+type hideConfig struct {
+	revealConfig
+	mnemonic string
+}
+
+type revealConfig struct {
+	imgPath string
+}
+
+func parseHideArgs(c *hideConfig, args []string, stderr io.Writer) error {
+	fs := flag.NewFlagSet("imgp", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "usage: %s hide [options]\n", fs.Name())
+		fs.PrintDefaults()
+	}
+
+	fs.StringVar(&c.imgPath, "image-path", "", "Path to .png file")
+	fs.StringVar(&c.mnemonic, "mnemonic", "", "12 word mnemonic (words separated by spaces)")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	if err := validateHideArgs(c); err != nil {
+		fmt.Fprintln(fs.Output(), err)
+		fs.Usage()
+		return err
+	}
+
+	return nil
+}
+
+func validateHideArgs(c *hideConfig) error {
+	if c.imgPath == "" {
+		return argError(c.imgPath, "flag -image-path", errors.New("should not be empty"))
+	}
+
+	if c.mnemonic == "" {
+		return argError(c.mnemonic, "flag -mnemonic", errors.New("should not be empty"))
+	}
+
+	if words := strings.Fields(c.mnemonic); len(words) != 12 {
+		return argError(c.mnemonic, "mnemonic", errors.New("should be 12 words"))
+	}
+
+	return nil
+}
+
+func parseRevealArgs(c *revealConfig, args []string, stderr io.Writer) error {
+	fs := flag.NewFlagSet("imgp", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "usage: %s reveal [options]\n", fs.Name())
+		fs.PrintDefaults()
+	}
+
+	fs.StringVar(&c.imgPath, "image-path", "", "Path to modified .png file")
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	if err := validateRevealArgs(c); err != nil {
+		fmt.Fprintln(fs.Output(), err)
+		fs.Usage()
+		return err
+	}
+
+	return nil
+}
+
+func validateRevealArgs(c *revealConfig) error {
+	if c.imgPath == "" {
+		return argError(c.imgPath, "flag -image-path", errors.New("should not be empty"))
+	}
+
+	return nil
+}
+
+func argError(value any, arg string, err error) error {
+	return fmt.Errorf(`invalid value "%v" for %s: %w`, value, arg, err)
+}
--- cmd/imgp/imgp.go
+++ cmd/imgp/imgp.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	HideCommand     = "hide"
+	RevealCommand   = "reveal"
+	SubcommandError = "expected 'hide' or 'reveal' subcommand"
+)
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	e := &env{
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+		args:   os.Args,
+	}
+
+	if err := run(e); err != nil {
+		fmt.Fprintln(e.stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(e *env) error {
+	if len(e.args) < 2 {
+		return fmt.Errorf(SubcommandError)
+	}
+
+	switch e.args[1] {
+	case HideCommand:
+		c := hideConfig{}
+
+		if err := parseHideArgs(&c, e.args[2:], e.stderr); err != nil {
+			return err
+		}
+
+		return runHide(e, &c)
+	case RevealCommand:
+		c := revealConfig{}
+
+		if err := parseRevealArgs(&c, e.args[2:], e.stderr); err != nil {
+			return err
+		}
+
+		return runReveal(e, &c)
+	default:
+		return fmt.Errorf(SubcommandError)
+	}
+}
+
+func runHide(e *env, c *hideConfig) error {
+	fmt.Fprintf(e.stdout, "Image Path:\t%s\n", c.imgPath)
+	fmt.Fprintf(e.stdout, "Mnemonic:\t%s\n", c.mnemonic)
+
+	return nil
+}
+
+func runReveal(e *env, c *revealConfig) error {
+	fmt.Fprintf(e.stdout, "Image Path:\t%s\n", c.imgPath)
+
+	return nil
 }
